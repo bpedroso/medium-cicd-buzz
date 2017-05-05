f7e412cc3174eee091c1511e7cffffc3ecee308c
brunopedrosoc4 <bruno_pedroso@carrefour.com>
2017-05-04 21:52:42 -0300

    Add dependencies flask-restfull

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import signal
 from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 from api import generator
 
 app = Flask(__name__)
+api = Api(app)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
 
 @app.route("/")
 def generate_buzz():
